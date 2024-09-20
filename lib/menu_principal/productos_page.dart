2ff9f57bdcb729f1aa1dc7fda9d3b9a9377cbe72
Santiago Villabona <88673966+MeperDonas@users.noreply.github.com>
2024-09-20 14:03:51 -0500

    si

--- lib/menu_principal/productos_page.dart
+++ lib/menu_principal/productos_page.dart
@@ -1,5 +1,4 @@
 import 'package:app/carro_compra/carrito_page.dart';
-import 'package:app/menu_principal/ver_productos.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 
