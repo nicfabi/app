7580fc83b8960bd87e9fa05695483985b620db01
Sofía princess <sofusaka@gmail.com>
2024-10-16 19:01:30 -0500

    ADD CLIENTES
    AHORA SE PUEDEN ENVIAR FACTURA A CLIENTES!!!!

--- lib/pago_page.dart
+++ lib/pago_page.dart
@@ -7,36 +7,95 @@ class PagoPage extends GetView<CarritoService> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pago', style: TextStyle(color: Color(0xFFFAFAFA))),
+        title: const Text(
+          'Pago',
+          style: TextStyle(color: Color(0xFFFAFAFA)),
+        ),
         backgroundColor: const Color(0xFF09184D),
         centerTitle: true,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Obx(() => Text(
-                  'Total a pagar: \$${controller.total.toStringAsFixed(2)}',
-                  style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.bold),
-                )),
-            const SizedBox(height: 20),
-            Obx(() {
-              if (controller.total > 0.00) {
-                return ElevatedButton(
-                  child: const Text('Realizar Pago'),
-                  onPressed: () {
-                    controller.agregarCarrito(context);
-                    Get.snackbar('Pago', 'Pago realizado con éxito');
-                    Get.snackbar(
-                        'Factura Enviada', "Factura enviada con éxito!");
-                    controller.limpiarCarrito();
-                  },
-                );
-              } else {
-                return Container(); // or any other widget you want to return when the condition is not met
-              }
-            })
+            Obx(
+              () => Text(
+                'Total a pagar: \$${controller.total.toStringAsFixed(2)}',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF09184D),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Obx(
+              () {
+                if (controller.total > 0.00) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Ingrese el ID del cliente',
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 20,
+                          ),
+                        ),
+                        onChanged: (value) {
+                          controller.setClienteSeleccionado(value);
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          backgroundColor: const Color(0xFF09184D),
+                        ),
+                        child: const Text(
+                          'Realizar Pago',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                        onPressed: () {
+                          
+                          
+                            controller.agregarCarrito(
+                              context, controller.clienteSeleccionado);
+                            Get.snackbar('Pago', 'Pago realizado con éxito',
+                                snackPosition: SnackPosition.BOTTOM);
+                            Get.snackbar(
+                              'Factura Enviada',
+                              "Factura enviada con éxito!",
+                              snackPosition: SnackPosition.BOTTOM,
+                            );
+                            controller.limpiarCarrito();
+                          
+                        },
+                      ),
+                    ],
+                  );
+                } else {
+                  return const Text(
+                    'No hay productos en el carrito para pagar',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  );
+                }
+              },
+            ),
           ],
         ),
       ),
